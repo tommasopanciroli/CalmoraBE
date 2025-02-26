b0b788cac0310b01ce33f296e63b6e3fcb103008
tommaso.panciroli <tommypanci@gmail.com>
2025-02-27 00:08:12 +0100

    initialized project and insert the missing dependecies on pom.xml

---
+++ src/test/java/com/example/Calmora/CalmoraBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Calmora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalmoraBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
