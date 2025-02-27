72d6a6dbbfc3142d6df4695e8a829c90b0ef8c38
tommaso.panciroli <tommypanci@gmail.com>
2025-02-27 17:17:47 +0100

    updated dependencies in pom.xml and adds auth repository

--- src/main/java/com/example/Calmora/CalmoraBeApplication.java
+++ src/main/java/com/example/Calmora/CalmoraBeApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CalmoraBeApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CalmoraBeApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CalmoraBeApplication.class, args);
+    }
 
 }
